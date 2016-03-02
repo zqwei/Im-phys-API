3bdaa5bcc17de07b651f2fa39cc12a04c18a30d2
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-03-02 15:51:52 -0500

    only plots code is kept in exampleNeuron, the computation is done on server __WS3__

--- ModelsExploration/exampleNeuron.m
+++ ModelsExploration/exampleNeuron.m
@@ -12,75 +12,90 @@ if ~exist([PlotDir 'ModelCellFits'],'dir')
     mkdir([PlotDir 'ModelCellFits'])
 end
 
-paras = repmat(struct('cellName',1, 'nRep', 1, 'expression', 'virus',...
-                        'CaIndicator', 'GCaMP6f', 'n_fast', 1, 'n_smc', 1, ...
-                        'Fm',1, 'K', 1, 'n', 1, 'tau_r', 1, 'tau_d', 1),length(totCell), 1); 
-                    
-                    
-                    
+% paras = repmat(struct('cellName',1, 'nRep', 1, 'expression', 'virus',...
+%                         'CaIndicator', 'GCaMP6f', 'n_fast', 1, 'n_smc', 1, ...
+%                         'Fm',1, 'K', 1, 'n', 1, 'tau_r', 1, 'tau_d', 1),length(totCell), 1); 
+%                     
+%                     
+%                     
+% for nCell   = 1:length(totCell)  
+%     %% S2C model
+%     tic
+%     
+%     spk          = totCell(nCell).spk;
+%     dff          = totCell(nCell).dff;
+%     if isa(dff, 'single'); dff = double(dff); end
+%     para_start   = [20   20.4788    1.1856    1    0.2107];
+%     t_frame      = totCell(nCell).CaTime;
+%     
+%     %%%%%%%% linear model
+%     % para_start                   = [20   0    1    0.2107];
+%     % para_final                   = gcamp6_linear_model({spk}, dff, para.t_frame, para_start);
+%     % CaTraces                     = func_getCaTraces_linear({spk},para.t_frame,para_final);
+%     
+%     %%%%%%%% quadratic model
+%     % para_start                   = [20  0  0  1  0.2107];
+%     % para_final                   = gcamp6_quadratic_model({spk}, dff, para.t_frame, para_start);
+%     % CaTraces                     = func_getCaTraces_quadratic({spk},para.t_frame,para_final);
+%     
+%     % Hill model
+%     para_final   = gcamp6_model_4para_new({spk}, dff, t_frame, para_start);
+%     fitCaTraces  = func_getCaTraces_general_new({spk}, t_frame,para_final);
+%     paras(nCell).cellName      = totCell(nCell).cellName;
+%     paras(nCell).nRep          = totCell(nCell).nRep;
+%     paras(nCell).expression    = totCell(nCell).expression;
+%     paras(nCell).CaIndicator   = totCell(nCell).CaIndicator;
+%     paras(nCell).Fm            = para_final(1);
+%     paras(nCell).K             = para_final(2);
+%     paras(nCell).n             = para_final(3);
+%     paras(nCell).tau_d         = para_final(4);
+%     paras(nCell).tau_r         = para_final(5);
+%  
+%     %% C2S model
+%     
+%     V.fast_iter_max    = 1000;
+%     V.smc_iter_max     = 1000;
+%     V.dt               = t_frame(end)/length(t_frame);
+%     V.preprocess       = 1; % high-pass filter (increase fitting speed)
+%     V.T                = length(t_frame);
+%     [fast, smc]        = runOOPSI(dff', V);
+%     n_fast             = fast.n/max(fast.n);
+%     paras(nCell).n_fast = n_fast;
+%     if ~isempty(smc)
+%         n_smc  = smc.E.nbar;
+%     else
+%         n_smc  = n_fast;
+%     end
+%     n_smc  = n_smc/max(n_smc);
+%     paras(nCell).n_smc  = n_smc;
+% 
+%             
+% end
+% 
+% save([TempDatDir 'ParamsFitCells.mat'], 'paras');
+
+
+
+%% This code is only for plots, where computation is done by server
+
+load([TempDatDir 'ParamsFitCells.mat'], 'paras');
+
 for nCell   = 1:length(totCell)  
-    %% S2C model
-    tic
     
     spk          = totCell(nCell).spk;
     dff          = totCell(nCell).dff;
     if isa(dff, 'single'); dff = double(dff); end
-    para_start   = [20   20.4788    1.1856    1    0.2107];
-    t_frame      = totCell(nCell).CaTime;
-    
-    %%%%%%%% linear model
-    % para_start                   = [20   0    1    0.2107];
-    % para_final                   = gcamp6_linear_model({spk}, dff, para.t_frame, para_start);
-    % CaTraces                     = func_getCaTraces_linear({spk},para.t_frame,para_final);
-    
-    %%%%%%%% quadratic model
-    % para_start                   = [20  0  0  1  0.2107];
-    % para_final                   = gcamp6_quadratic_model({spk}, dff, para.t_frame, para_start);
-    % CaTraces                     = func_getCaTraces_quadratic({spk},para.t_frame,para_final);
-    
-    % Hill model
-    para_final   = gcamp6_model_4para_new({spk}, dff, t_frame, para_start);
+    t_frame      = totCell(nCell).CaTime;    
+    para_final   = [paras(nCell).Fm paras(nCell).K paras(nCell).n paras(nCell).tau_d paras(nCell).tau_r];
     fitCaTraces  = func_getCaTraces_general_new({spk}, t_frame,para_final);
-    paras(nCell).cellName      = totCell(nCell).cellName;
-    paras(nCell).nRep          = totCell(nCell).nRep;
-    paras(nCell).expression    = totCell(nCell).expression;
-    paras(nCell).CaIndicator   = totCell(nCell).CaIndicator;
-    paras(nCell).Fm            = para_final(1);
-    paras(nCell).K             = para_final(2);
-    paras(nCell).n             = para_final(3);
-    paras(nCell).tau_d         = para_final(4);
-    paras(nCell).tau_r         = para_final(5);
-    
-    toc
-    
-    tic
-    
-    %% C2S model
-    
-    V.fast_iter_max    = 1000;
-    V.smc_iter_max     = 1000;
-    V.dt               = t_frame(end)/length(t_frame);
-    V.preprocess       = 1; % high-pass filter (increase fitting speed)
-    V.T                = length(t_frame);
-    [fast, smc]        = runOOPSI(dff', V);
-    n_fast             = fast.n/max(fast.n);
-    paras(nCell).n_fast = n_fast;
-    if ~isempty(smc)
-        n_smc  = smc.E.nbar;
-    else
-        n_smc  = n_fast;
-    end
-    n_smc  = n_smc/max(n_smc);
-    paras(nCell).n_smc  = n_smc;
 
-    toc        
-            
-    
-    %% plots
     normalized_dff     = (dff - min(dff))/(max(dff)-min(dff));
     normalized_fitCaTraces = (fitCaTraces - min(dff))/(max(dff)-min(dff));
     n_spk              = hist(spk, t_frame);
     n_spk              = n_spk/max(n_spk);
+    
+    n_fast             = paras(nCell).n_fast;
+    n_smc              = paras(nCell).n_smc; 
     
     figure;
     
@@ -131,7 +146,5 @@ for nCell   = 1:length(totCell)
     setPrint(18, 10,[PlotDir 'ModelCellFits/' totCell(nCell).expression '_' ...
         totCell(nCell).cellName '_' num2str(totCell(nCell).nRep,'%02d')])
 end
-
-save([TempDatDir 'ParamsFitCells.mat'], 'paras');
 
 close all;
