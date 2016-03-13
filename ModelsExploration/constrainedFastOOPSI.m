5dd3457df504ff652459e9acd09e28b5f4674d87
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-03-12 23:29:52 -0500

    remove fast-oopsi test code to test git -- see https://github.com/zqwei/Ca-Imaging-Deconv-List

--- ModelsExploration/constrainedFastOOPSI.m
+++
@@ -1,4 +0,0 @@
-function constrainedFastOOPSI(dff, para)
-    normalized_dff               = (dff - min(dff))/(max(dff) - min(dff));
-    SAMP                         = cont_ca_sampler(normalized_dff);
-    plot_continuous_samples(SAMP, normalized_dff, para);
