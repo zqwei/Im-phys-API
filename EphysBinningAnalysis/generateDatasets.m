b3a2b9c4bce90de8e85d7ab19eaccdd1a0c504da
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-04-09 23:34:24 -0400

    update generate datasets for different binning of ephys

--- EphysBinningAnalysis/generateDatasets.m
+++ EphysBinningAnalysis/generateDatasets.m
@@ -27,21 +27,20 @@ DataSetList(1).ActiveNeuronIndex = findHighFiringUnits(nDataSet, params, minFiri
 save([TempDatDir DataSetList(1).name '.mat'], 'nDataSet');
 
 
-params.binsize         =  0.067;
-params.frameRate       =  1000; %Hz -- smallest bin is 0.001
+params.binsize         =  0.070;
+params.frameRate       =  200; %Hz -- smallest bin is 0.001
 minTimeToAnalysis      =  round(-3.1 * params.frameRate);
 maxTimeToAnalysis      =  round(2.0 * params.frameRate);
 params.timeWindowIndexRange  = minTimeToAnalysis : maxTimeToAnalysis;
-params.timeSeries      = params.timeWindowIndexRange * 0.001;
+params.timeSeries      = params.timeWindowIndexRange / params.frameRate;
 nDataSet               =  getSpikeDataWithEphysTimeAndBoxCar(SpikingDataDir, SpikeFileList, params.minNumTrialToAnalysis, params.timeSeries, params.binsize);                                  
-DataSetList(2).name    = 'Shuffle_Spikes_boxcar_067ms';
+DataSetList(2).name    = 'Shuffle_Spikes_boxcar_070ms';
 DataSetList(2).params  = params; 
 DataSetList(2).ActiveNeuronIndex = findHighFiringUnits(nDataSet, params, minFiringRate);
 save([TempDatDir DataSetList(2).name '.mat'], 'nDataSet', '-v7.3');
 
 
 params.binsize         =  0.100;
-params.frameRate       =  1/params.binsize;
 nDataSet               =  getSpikeDataWithEphysTimeAndBoxCar(SpikingDataDir, SpikeFileList, params.minNumTrialToAnalysis, params.timeSeries, params.binsize);                                  
 DataSetList(3).name    = 'Shuffle_Spikes_boxcar_100ms';
 DataSetList(3).params  = params; 
@@ -49,7 +48,6 @@ DataSetList(3).ActiveNeuronIndex = findHighFiringUnits(nDataSet, params, minFiri
 save([TempDatDir DataSetList(3).name '.mat'], 'nDataSet', '-v7.3');
 
 params.binsize         =  0.150;
-params.frameRate       =  1/params.binsize;
 nDataSet               =  getSpikeDataWithEphysTimeAndBoxCar(SpikingDataDir, SpikeFileList, params.minNumTrialToAnalysis, params.timeSeries, params.binsize);                                  
 DataSetList(4).name    = 'Shuffle_Spikes_boxcar_150ms';
 DataSetList(4).params  = params; 
@@ -57,7 +55,6 @@ DataSetList(4).ActiveNeuronIndex = findHighFiringUnits(nDataSet, params, minFiri
 save([TempDatDir DataSetList(4).name '.mat'], 'nDataSet', '-v7.3');
 
 params.binsize         =  0.200;
-params.frameRate       =  1/params.binsize;
 nDataSet               =  getSpikeDataWithEphysTimeAndBoxCar(SpikingDataDir, SpikeFileList, params.minNumTrialToAnalysis, params.timeSeries, params.binsize);                                  
 DataSetList(5).name    = 'Shuffle_Spikes_boxcar_200ms';
 DataSetList(5).params  = params; 
@@ -65,7 +62,6 @@ DataSetList(5).ActiveNeuronIndex = findHighFiringUnits(nDataSet, params, minFiri
 save([TempDatDir DataSetList(5).name '.mat'], 'nDataSet', '-v7.3');
 
 params.binsize         =  0.250;
-params.frameRate       =  1/params.binsize;
 nDataSet               =  getSpikeDataWithEphysTimeAndBoxCar(SpikingDataDir, SpikeFileList, params.minNumTrialToAnalysis, params.timeSeries, params.binsize);                                  
 DataSetList(6).name    = 'Shuffle_Spikes_boxcar_250ms';
 DataSetList(6).params  = params; 
