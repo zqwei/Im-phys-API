b6c4d01cab8b3344f7fc71857a8ab15efd6e3f90
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-04-16 15:13:30 -0400

    add code for ephys binning effect analysis

---
+++ ExampleNeuron/examplePeakingNeuron.m
@@ -0,0 +1,7 @@
+
+peakingNeuronIndex = [97 244 378 441 459 236 354 387 457 492 239 370 436 ...
+    510 604 615 654 714 771 779 817 828 872 875 957 995];
+
+
+load ([TempDatDir 'DataListShuffle.mat']);
+rawDataSetList = DataSetList;
