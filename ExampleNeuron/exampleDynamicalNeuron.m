ba6c736afdcbb0bdf139dd732047c457bfb5fbdd
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-04-19 01:35:57 -0400

    update example neuron of dynamical neuron plot with raster plots

--- ExampleNeuron/exampleDynamicalNeuron.m
+++ ExampleNeuron/exampleDynamicalNeuron.m
@@ -1,66 +1,145 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Cell type categorization
-% homogenous cell        : no change of selectivity
-% heterogenous cell      : change of selectivity
-% no-selective cell      : no selectivity
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-addpath('../Func');
-setDir;
-load ([TempDatDir 'DataListShuffle.mat']);
+function exampleDynamicalNeuron
+    addpath('../Func');
+    setDir;
+    
+    load ([TempDatDir 'DataListShuffle.mat']);
+    nData = 1; % plot raster and psth
+    load([TempDatDir DataSetList(nData).name '.mat'])
+    spikeDataSet = nDataSet;   
+    params = DataSetList(nData).params;
+    nData = 4; % plot ca comparison
+    load([TempDatDir DataSetList(nData).name '.mat'])
+    caDataSet    = nDataSet;
+    load ([TempDatDir 'DataListS2CModel.mat']);    
+    nData = 1; % plot s2c model
+    load([TempDatDir DataSetList(nData).name '.mat'])
+    s2cDataSet   = nDataSet;
+     
+    meanCaDataSet = meanData(caDataSet);
+    meanS2CDataSet = meanData(s2cDataSet);
 
+    dynamicalNeuronIndex = [334 177 142 27 30 80 93 580 557 367];
+    
+    for nCell = dynamicalNeuronIndex
+        figure;
+        % spike
+        subplot(2, 3, 1)
+        plotRaster(spikeDataSet(nCell), params);
+        subplot(2, 3, 4)
+        plotPSTH(spikeDataSet(nCell), params, 'Firing rate (Hz)');
+        
+        %
+        subplot(2, 3, 3)
+        plotDff(s2cDataSet(nCell), params)
+        subplot(2, 3, 6)
+        plotPSTH(s2cDataSet(nCell), params, 'DF/F');
+        
+        mCell = findSimilarCellToS2CModel(meanS2CDataSet(nCell,:), meanCaDataSet);
+        subplot(2, 3, 2)
+        plotDff(caDataSet(mCell), params)
+        subplot(2, 3, 5)
+        plotPSTH(caDataSet(mCell), params, 'DF/F');
+        setPrint(8*3, 6*2, [PlotDir 'SingleUnitsTscore/SingleUnitsTscoreExampleNeuron_' num2str(nCell, '%04d')])
+        close all
+    end
 
-if ~exist([PlotDir 'SingleUnitsTscore'],'dir')
-    mkdir([PlotDir 'SingleUnitsTscore'])
 end
 
-numExample             = 4;
-
-color_index   = [0.7 0.0 0.0; % CL
-                 0.0 0.0 0.7; % CR
-                 1.0 0.7 0.7; % EL
-                 0.7 0.7 1.0]; % ER
-
-nData         = 1;
-load([TempDatDir DataSetList(nData).name '.mat'])             
-sigma                         = 0.15 / DataSetList(1).params.binsize; % 200 ms
-filterLength                  = 11;
-filterStep                    = linspace(-filterLength / 2, filterLength / 2, filterLength);
-filterInUse                   = exp(-filterStep .^ 2 / (2 * sigma ^ 2));
-filterInUse                   = filterInUse / sum (filterInUse);  
-textYLabels                   = {'Firing rate (Hz)', 'DF/F', 'DF/F', 'DF/F'};
-
-for nData              = [1 3 4]
-    load([TempDatDir 'LogPValueTscore_' DataSetList(nData).name '.mat'], 'logPValueEpoch')
-    load([TempDatDir DataSetList(nData).name '.mat'])             
-
-    unitGroup = plotTtestLogPSpikeEpoch (logPValueEpoch);
-
-    figure;
-    
-    for nGroup         = 1:2
-        neuronIndice   = find(unitGroup==nGroup);
-        for nExample   = 1:numExample
-            neuronIndex = neuronIndice(nExample);
-            
-            subplot(2, 4, nExample + (nGroup-1)*4)
-            hold on;
-            nUnitData        = nDataSet(neuronIndex).unit_yes_trial;
-            smoothedUnitData = getGaussianPSTH (filterInUse, nUnitData, 2);
-            shadedErrorBar(DataSetList(nData).params.timeSeries, mean(smoothedUnitData, 1),...
-            std(smoothedUnitData, 1)/sqrt(size(smoothedUnitData, 1)),...
-            {'k-', 'linewid', 1.0, 'color', color_index(1, :)}, 0.5);
-            nUnitData        = nDataSet(neuronIndex).unit_no_trial;
-            smoothedUnitData = getGaussianPSTH (filterInUse, nUnitData, 2);
-            shadedErrorBar(DataSetList(nData).params.timeSeries, mean(smoothedUnitData, 1),...
-            std(smoothedUnitData, 1)/sqrt(size(smoothedUnitData, 1)),...
-            {'-', 'linewid', 1.0, 'color', color_index(2, :)}, 0.5);
-            gridxy ([DataSetList(nData).params.polein, DataSetList(nData).params.poleout, 0],[], 'Color','k','Linestyle','--','linewid', 1.0)
-            hold off;
-            ylabel(textYLabels{nData});
-            xlabel('Time (s)');
-            xlim([DataSetList(nData).params.timeSeries(1) DataSetList(nData).params.timeSeries(end)]);
+function plotRaster(spikeDataSet, params)
+    spkTimes{1}    = spikeDataSet.unit_yes_trial_spk_time;
+    spkTimes{2}    = spikeDataSet.unit_no_trial_spk_time;
+    hold on;
+    color_index    = [0.7  0 0; 0 0 0.7];
+    spkLoc         = 0;
+    for nPlot            = 1:2
+        hold on;
+        for nTrial       = 1:20
+            spkLoc       = spkLoc + 1;
+            spikeTimes   = spkTimes{nPlot}{nTrial};
+            spikeTrial   = ones(length(spikeTimes), 1) * spkLoc;
+            plot(spikeTimes, spikeTrial, '.', 'color', color_index(nPlot, :));
         end
+        spkLoc     = spkLoc + 1;%length(spkTimes{nPlot}) + 3;
     end
-    setPrint(8*4, 6*2, [PlotDir 'SingleUnitsTscore/SingleUnitsTscoreExampleNeuron_' DataSetList(nData).name], 'svg')
+
+    gridxy ([params.polein, params.poleout, 0],[], 'Color','k','Linestyle','--','linewid', 1.0);
+    hold off;
+    ylim([1 41])
+    xlim([params.timeSeries(1) params.timeSeries(end)]);
+    axis off
+end
+
+function plotDff(spikeDataSet, params)
+    sigma                         = 0.15 / params.binsize; % 200 ms
+    filterLength                  = 11;
+    filterStep                    = linspace(-filterLength / 2, filterLength / 2, filterLength);
+    filterInUse                   = exp(-filterStep .^ 2 / (2 * sigma ^ 2));
+    filterInUse                   = filterInUse / sum (filterInUse); 
+
+    nUnitData        = spikeDataSet.unit_yes_trial;
+    yesUnitData      = getGaussianPSTH (filterInUse, nUnitData, 2);
+    nUnitData        = spikeDataSet.unit_no_trial;
+    noUnitData       = getGaussianPSTH (filterInUse, nUnitData, 2);
+    cmin             = min([mean(yesUnitData), mean(noUnitData)]);
+    cmax             = max([mean(yesUnitData), mean(noUnitData)]);
+    
+
+
+    hold on
+    actMat = nan(41, size(spikeDataSet.unit_yes_trial, 2));
+    actMat(1:20, :) = yesUnitData(1:20, :);
+    actMat(22:end, :) = noUnitData(1:20, :);
+    imagesc(params.timeSeries, 1:41, actMat);
+    gridxy ([params.polein, params.poleout, 0],[], 'Color','w','Linestyle','--','linewid', 1.0);
+    hold off;
+    ylim([1 41])
+    colormap(gray)
+    
+    caxis([cmin, cmax]);
+    colorbar
+    xlim([params.timeSeries(1) params.timeSeries(end)]);
+    axis off
+end
+
+
+function plotPSTH(spikeDataSet, params, ylabelName)
+    sigma                         = 0.15 / params.binsize; % 200 ms
+    filterLength                  = 11;
+    filterStep                    = linspace(-filterLength / 2, filterLength / 2, filterLength);
+    filterInUse                   = exp(-filterStep .^ 2 / (2 * sigma ^ 2));
+    filterInUse                   = filterInUse / sum (filterInUse); 
+
+    color_index    = [0.7  0 0; 0 0 0.7];
+    hold on;
+    nUnitData        = spikeDataSet.unit_yes_trial;
+    nUnitData        = getGaussianPSTH (filterInUse, nUnitData, 2);
+    shadedErrorBar(params.timeSeries, mean(nUnitData, 1),...
+        std(nUnitData, 1)/sqrt(size(nUnitData, 1)),...
+        {'k-', 'linewid', 1.0, 'color', color_index(1, :)}, 0.5);
+    nUnitData        = spikeDataSet.unit_no_trial;
+    nUnitData        = getGaussianPSTH (filterInUse, nUnitData, 2);
+    shadedErrorBar(params.timeSeries, mean(nUnitData, 1),...
+        std(nUnitData, 1)/sqrt(size(nUnitData, 1)),...
+        {'k-', 'linewid', 1.0, 'color', color_index(2, :)}, 0.5);
+    gridxy ([params.polein, params.poleout, 0],[], 'Color','k','Linestyle','--','linewid', 1.0)
+    hold off;
+    ylabel(ylabelName);
+    xlabel('Time (s)');
+    xlim([params.timeSeries(1) params.timeSeries(end)]);
+    set(gca, 'TickDir', 'out')
+end
+
+
+function meanDataSet = meanData(nDataSet)
+    meanDataSet = nan(length(nDataSet), size(nDataSet(1).unit_yes_trial, 2)*2);
+    for nCell = 1:length(nDataSet)
+        meanDataSet(nCell, :) = [mean(nDataSet(nCell).unit_yes_trial), mean(nDataSet(nCell).unit_no_trial)];
+    end
+end
+
+function mIndex = findSimilarCellToS2CModel(nCellS2CDataSet, caDataSet)
+    corrDat     = corr(nCellS2CDataSet', caDataSet');
+    [~, mIndex] = max(corrDat);
+    mIndex      = mIndex(1);
 end
