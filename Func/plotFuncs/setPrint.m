3149f0b252ee0260034b22feaae4643297f2e8fb
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-04-20 11:42:52 -0400

    add print with EPS and vectorized plot

--- Func/plotFuncs/setPrint.m
+++ Func/plotFuncs/setPrint.m
@@ -5,15 +5,17 @@ function setPrint(width, height, fname, printType)
 
     if nargin == 3
         % printType = 'svg';
-        printType = 'tif';
+        printType = 'eps';
     end
-
+    set(gcf, 'renderer', 'painters', 'renderermode', 'manual');
     set(gcf,'PaperUnits','centimeters');
     set(gcf,'PaperSize',[width height]);
     set(gcf,'PaperPosition',[0 0 width height]);
     box off;
     
     switch upper(printType)
+        case 'EPS'
+            print('-depsc',[fname '.eps'])
         case 'PNG'
             print('-dpng',[fname '.png'])
         case 'PDF'
