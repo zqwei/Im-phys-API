537a3c671743fd8633c48e84630c0f99678e787b
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-05-05 11:02:52 -0400

    add analysis based on raw data with outlier removal in Ca++ dataset

---
+++ Ca++WithOutlierRemoval/noiseCorrelationEpochs.m
@@ -0,0 +1,147 @@
+
+function noiseCorrelationEpochs
+    addpath('../Func');
+    setDir;
+    load ([TempDatDir 'DataListShuffle.mat']);
+    for nData   = [1 3 4]
+        load([TempDatDir DataSetList(nData).name '.mat'])
+        [yesTrialSignalCorr, yesTrialNoiseCorr, noTrialSignalCorr, noTrialNoiseCorr, yes_no_pair]...
+            = computeSignalCorrAndNoiseCorrEpochs(nDataSet, DataSetList(nData).params);
+        
+        figure;
+        titles = {'Sample', 'Delay', 'Response'};
+        for nPlot = 1:3
+            subplot(4, 3, nPlot)
+            [f1, xi1] = hist(yesTrialSignalCorr(:, nPlot), 1000);
+            [f2, xi2] = hist(noTrialSignalCorr(:, nPlot), 1000);
+            plot(xi1, f1/sum(f1), '-b', xi2, f2/sum(f2), '-r', 'linewid', 1.0)
+            ylabel('Frac.')
+            xlabel('Signal corr.')
+            box off
+            title(titles{nPlot})
+            
+            subplot(4, 3, nPlot+3)
+            [f1, xi1] = hist(yesTrialNoiseCorr(:, nPlot), 1000);
+            [f2, xi2] = hist(noTrialNoiseCorr(:, nPlot), 1000);
+            plot(xi1, f1/sum(f1), '-b', xi2, f2/sum(f2), '-r', 'linewid', 1.0)
+            ylabel('Frac.')
+            xlabel('Noise corr.')
+            box off
+            
+            subplot(4, 3, nPlot+3*2)
+            hold on
+            plotData = [yesTrialSignalCorr(yes_no_pair(:, 1), nPlot), noTrialSignalCorr(yes_no_pair(:, 2), nPlot)];
+            [N, C] = hist3(plotData, [30, 30]);
+            N(N==0) = nan;
+            b = imagesc(C{1}, C{2}, log10(N/length(yes_no_pair)));
+            set(b,'AlphaData',~isnan(N));
+            colorbar;
+            axis xy;
+            plot([-1 1], [-1 1], '--k', 'linewid', 1.0)
+            plot([1 -1], [-1 1], '--k', 'linewid', 1.0)
+            plot([0 0], [-1 1], '--k', 'linewid', 1.0)
+            plot([-1 1], [0 0], '--k', 'linewid', 1.0)
+            ylabel('Signal corr. (ipsi.)')
+            xlabel('Signal corr. (contra.)')
+            box off
+            hold off
+
+            
+            subplot(4, 3, nPlot+3*3)
+            hold on
+            plotData = [yesTrialNoiseCorr(yes_no_pair(:, 1), nPlot), noTrialNoiseCorr(yes_no_pair(:, 2), nPlot)];
+            [N, C] = hist3(plotData, [30, 30]);
+            N(N==0) = nan;
+            b = imagesc(C{1}, C{2}, log10(N/length(yes_no_pair)));
+            set(b,'AlphaData',~isnan(N));
+            colorbar;
+            axis xy;
+            plot([-1 1], [-1 1], '--k', 'linewid', 1.0)
+            plot([1 -1], [-1 1], '--k', 'linewid', 1.0)
+            plot([0 0], [-1 1], '--k', 'linewid', 1.0)
+            plot([-1 1], [0 0], '--k', 'linewid', 1.0)
+            ylabel('Noise corr. (ipsi.)')
+            xlabel('Noise corr. (contra.)')
+            box off
+            hold off
+
+            
+        end
+        
+        setPrint(8*3, 6*4, [PlotDir 'SingleUnitsNoiseCorrelation\' DataSetList(nData).name])
+        setPrint(8*3, 6*4, [PlotDir 'SingleUnitsNoiseCorrelation\' DataSetList(nData).name],'png')
+        
+    end
+end
+
+function [yesTrialSignalCorr, yesTrialNoiseCorr, noTrialSignalCorr, noTrialNoiseCorr, yes_no_pair]...
+            = computeSignalCorrAndNoiseCorrEpochs(nDataSet, params)
+    
+    numUnit            = length(nDataSet);
+    yesTrialSignalCorr = nan(numUnit, 3);
+    yesTrialNoiseCorr  = nan(numUnit, 3);
+    noTrialSignalCorr  = nan(numUnit, 3);
+    noTrialNoiseCorr   = nan(numUnit, 3);
+    yes_no_pair        = nan(numUnit, 2);
+    timePoints         = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);   
+    
+    nPairYes           = 0;
+    nPairNo            = 0;
+    nPairYesNo         = 0;
+    allSessionInd      = [nDataSet.sessionIndex];
+    sessionInd         = unique(allSessionInd);
+    
+    for nSession       = 1:length(sessionInd)
+        nSessInd       = sessionInd(nSession);
+        if sum(allSessionInd==nSessInd) > 1 % session with pairs
+            unitInd    = find(allSessionInd==nSessInd);
+            for nUnit  = 1:length(unitInd)
+                for mUnit = nUnit+1:length(unitInd)
+                    % yes trial
+                    % find pairs
+                    nUnitIdx      = unitInd(nUnit);
+                    mUnitIdx      = unitInd(mUnit);
+                    nUnitTrialInd = nDataSet(nUnitIdx).unit_yes_trial_index;
+                    mUnitTrialInd = nDataSet(mUnitIdx).unit_yes_trial_index;
+                    [C1, ai, bi]   = intersect(nUnitTrialInd, mUnitTrialInd);
+                    if length(C1) > 10
+                        nPairYes  = nPairYes + 1;
+                        yesTrialN = nDataSet(nUnitIdx).unit_yes_trial(ai, :);
+                        yesTrialM = nDataSet(mUnitIdx).unit_yes_trial(bi, :);
+                        for nPeriod = 1:3
+                            yesTrialNPeriod = yesTrialN(:,timePoints(nPeriod+1):timePoints(nPeriod+2));
+                            yesTrialMPeriod = yesTrialM(:,timePoints(nPeriod+1):timePoints(nPeriod+2));
+                            yesTrialSignalCorr(nPairYes, nPeriod) = corr(mean(yesTrialNPeriod, 1)', mean(yesTrialMPeriod, 1)');
+                            yesTrialNoiseCorr(nPairYes, nPeriod)  = corr(mean(yesTrialNPeriod, 2), mean(yesTrialMPeriod, 2));
+                        end
+                    end
+                    
+                    % no trial
+                    % find pairs
+                    nUnitIdx      = unitInd(nUnit);
+                    mUnitIdx      = unitInd(mUnit);
+                    nUnitTrialInd = nDataSet(nUnitIdx).unit_no_trial_index;
+                    mUnitTrialInd = nDataSet(mUnitIdx).unit_no_trial_index;
+                    [C2, ai, bi]   = intersect(nUnitTrialInd, mUnitTrialInd);
+                    if length(C2) > 10
+                        nPairNo  = nPairNo + 1;
+                        noTrialN = nDataSet(nUnitIdx).unit_no_trial(ai, :);
+                        noTrialM = nDataSet(mUnitIdx).unit_no_trial(bi, :);
+                        for nPeriod = 1:3
+                            noTrialNPeriod = noTrialN(:,timePoints(nPeriod+1):timePoints(nPeriod+2));
+                            noTrialMPeriod = noTrialM(:,timePoints(nPeriod+1):timePoints(nPeriod+2));
+                            noTrialSignalCorr(nPairNo, nPeriod) = corr(mean(noTrialNPeriod, 1)', mean(noTrialMPeriod, 1)');
+                            noTrialNoiseCorr(nPairNo, nPeriod)  = corr(mean(noTrialNPeriod, 2), mean(noTrialMPeriod, 2));
+                        end
+                    end
+                    
+                    if length(C1)>10 && length(C2)>10
+                        nPairYesNo         = nPairYesNo + 1;
+                        yes_no_pair(nPairYesNo, :) = [nPairYes, nPairNo];
+                    end
+                end
+            end
+        end
+    end
+    
+end
---
+++ Ca++WithOutlierRemoval/removeOutliersThompsonTau.m
@@ -0,0 +1,80 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Raw activity of all neurons
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+addpath('../Func');
+setDir;
+load ([TempDatDir 'DataListShuffle.mat']);
+
+% maxNumOutlier = 10;
+% 
+% 
+% for nData             = [3 4]
+%     load([TempDatDir DataSetList(nData).name '.mat'])
+%     for nUnit = 1:length(nDataSet)
+%         nCellDFF = nDataSet(nUnit).unit_yes_trial;
+%         X = nCellDFF;
+%         for nTime = 1:size(nCellDFF, 2)
+%             X(:, nTime) = thomspon_tau(nCellDFF(:, nTime), maxNumOutlier);
+%         end
+%         nDataSet(nUnit).unit_yes_trial_removeoutlier = X;
+%         
+%         nCellDFF = nDataSet(nUnit).unit_no_trial;
+%         X = nCellDFF;
+%         for nTime = 1:size(nCellDFF, 2)
+%             X(:, nTime) = thomspon_tau(nCellDFF(:, nTime), maxNumOutlier);
+%         end   
+%         nDataSet(nUnit).unit_no_trial_removeoutlier = X;
+%     end
+%     
+%     save([TempDatDir DataSetList(nData).name '.mat'], 'nDataSet')
+%    
+% end
+
+%%
+% remove those with above thres
+% remove the units with different presample activity
+
+thres          = 0.3;
+
+for nData              = [3 4]
+    load([TempDatDir DataSetList(nData).name '.mat'])
+    params             = DataSetList(nData).params;
+    timePeriod         = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
+    neuronRemoveList   = false(length(nDataSet), 1);
+    newDataSet         = rmfield(nDataSet, {'unit_no_trial_removeoutlier', 'unit_yes_trial_removeoutlier'});
+    for nUnit          = 1:length(nDataSet)
+        nCellData      = nDataSet(nUnit);
+        idx_yes_remove = mean(isnan(nCellData.unit_yes_trial_removeoutlier), 2)>thres;
+        idx_no_remove  = mean(isnan(nCellData.unit_no_trial_removeoutlier), 2)>thres; 
+        if sum(idx_yes_remove)>0 || sum(idx_no_remove)>0 
+            if sum(~idx_yes_remove)>20 && sum(~idx_no_remove)>20
+                nCellData.unit_yes_trial     = nCellData.unit_yes_trial(~idx_yes_remove, :);
+%                 nCellData.unit_yes_trial_raw = nCellData.unit_yes_trial_raw(~idx_yes_remove, :);
+                nCellData.unit_yes_trial_index = nCellData.unit_yes_trial_index(~idx_yes_remove);
+%                 nCellData.unit_yes_trial_removeoutlier = nCellData.unit_yes_trial_removeoutlier(~idx_yes_remove, :);
+                nCellData.unit_no_trial     = nCellData.unit_no_trial(~idx_no_remove, :);
+%                 nCellData.unit_no_trial_raw = nCellData.unit_no_trial_raw(~idx_no_remove, :);
+                nCellData.unit_no_trial_index = nCellData.unit_no_trial_index(~idx_no_remove);
+%                 nCellData.unit_no_trial_removeoutlier = nCellData.unit_no_trial_removeoutlier(~idx_no_remove, :);
+                nCellData = rmfield(nCellData, {'unit_no_trial_removeoutlier', 'unit_yes_trial_removeoutlier'});
+                if ~ttest2(mean(nCellData.unit_yes_trial(:, timePeriod(1):timePeriod(2))), mean(nCellData.unit_no_trial(:, timePeriod(1):timePeriod(2))))
+                    baseline        = nCellData.baseline;
+                    rawYes          = (nCellData.unit_yes_trial(:, timePeriod(1):timePeriod(2)) + 1) * baseline;
+                    rawNo           = (nCellData.unit_no_trial(:, timePeriod(1):timePeriod(2)) + 1) * baseline;
+                    meanPresample   = mean(mean([rawYes; rawNo]));
+                    nCellData.unit_yes_trial = (nCellData.unit_yes_trial +1)*baseline/meanPresample-1;
+                    nCellData.unit_no_trial  = (nCellData.unit_no_trial +1)*baseline/meanPresample-1;
+                    newDataSet(nUnit) = nCellData;
+                else
+                    neuronRemoveList(nUnit) = true;
+                end
+            else
+                neuronRemoveList(nUnit) = true;
+            end
+        end  
+    end
+    newDataSet = newDataSet(~neuronRemoveList);
+    nDataSet   = newDataSet;
+    save([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'], 'nDataSet', 'neuronRemoveList');
+end
+
