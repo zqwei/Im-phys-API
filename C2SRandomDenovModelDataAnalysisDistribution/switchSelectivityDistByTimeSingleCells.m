dde118cc5cc19c74e4c03cd8a77f86d2fc82fd95
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-06-13 13:22:12 -0400

    add plot code for cell type where the data is ran on sever

--- C2SRandomDenovModelDataAnalysisDistribution/switchSelectivityDistByTimeSingleCells.m
+++ C2SRandomDenovModelDataAnalysisDistribution/switchSelectivityDistByTimeSingleCells.m
@@ -13,41 +13,66 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 addpath('../Func');
 setDir;
-load ([TempDatDir 'DataListShuffle.mat']);
-numFold = 30;
+% % load ([TempDatDir 'DataListShuffle.mat']);
+% % numFold = 30;
+% % 
+% % % short Ca slow
+% % nData  = 3;
+% % params = DataSetList(nData).params;
+% % std_r                  = 0.0375; %0.0375;
+% % median_r               = 0.0927;
+% % std_d                  = 0.5374; %0.5374;
+% % median_d               = 1.2294;
+% % load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
+% % per_list               = 0.02:0.01:0.98;
+% % tau_d_list             = icdf('Normal', per_list, 0, 1) * std_d + median_d;
+% % noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
+% % numNeuron              = length(nDataSet);
+% % cellTypeMat            = nan(numNeuron, numFold, length(noise_factor_list), length(tau_d_list));
+% % for nTau              = 1:length(tau_d_list)
+% %     spikeDataSet      = getSyntheticSpikeDeconvDataSimpleVersion(nDataSet, median_r, tau_d_list(nTau), params);  
+% %     for nFF           = 1:length(noise_factor_list) 
+% %         for nFold     = 1:numFold
+% %             unitGroup = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
+% %             cellTypeMat(:, nFold, nFF, nTau) = unitGroup;
+% %         end
+% %     end
+% % end
+% % cellTypeMatGP43 = cellTypeMat;
+% % 
+% % % short Ca slow virus
+% % nData  = 4;
+% % params = DataSetList(nData).params;
+% % std_r                  = 0.0246;
+% % median_r               = 0.0505;
+% % std_d                  = 0.4588;
+% % median_d               = 1.7064;
+% % load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
+% % per_list               = 0.05:0.05:0.95;
+% % tau_d_list             = icdf('Normal', per_list, 0, 1) * std_d + median_d;
+% % noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
+% % 
+% % numNeuron              = length(nDataSet);
+% % 
+% % cellTypeMat            = nan(numNeuron, numFold, length(noise_factor_list), length(tau_d_list));
+% % 
+% % for nTau              = 1:length(tau_d_list)
+% %     spikeDataSet      = getSyntheticSpikeDeconvDataSimpleVersion(nDataSet, median_r, tau_d_list(nTau), params);  
+% %     for nFF           = 1:length(noise_factor_list) 
+% %         for nFold     = 1:numFold
+% %             unitGroup = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
+% %             cellTypeMat(:, nFold, nFF, nTau) = unitGroup;
+% %         end
+% %     end
+% % end
+% % cellTypeMat6sAAV = cellTypeMat;
 
-% short Ca slow
-nData  = 3;
-params = DataSetList(nData).params;
-std_r                  = 0.0375; %0.0375;
-median_r               = 0.0927;
-std_d                  = 0.5374; %0.5374;
-median_d               = 1.2294;
-load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
-per_list               = 0.02:0.01:0.98;
-tau_d_list             = icdf('Normal', per_list, 0, 1) * std_d + median_d;
-noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
-
-numNeuron              = length(nDataSet);
-
-cellTypeMat            = nan(numNeuron, numFold, length(noise_factor_list), length(tau_d_list));
-
-for nTau              = 1:length(tau_d_list)
-    spikeDataSet      = getSyntheticSpikeDeconvDataSimpleVersion(nDataSet, median_r, tau_d_list(nTau), params);  
-    for nFF           = 1:length(noise_factor_list) 
-        for nFold     = 1:numFold
-            unitGroup = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
-            cellTypeMat(:, nFold, nFF, nTau) = unitGroup;
-        end
-    end
-end
-
-cellTypeMatGP43 = cellTypeMat;
-
+load('cellTypeMat.mat')
+cellTypeMat = cellTypeMatGP43;
 % result for single neuron
 figure
 nNeuron = 100;
-imagesc(tau_d_list, noise_factor_list, squeeze(mean(cellTypeMat(nNeuron, :, :, :), 2)))
+imagesc(squeeze(mean(cellTypeMat(nNeuron, :, :, :), 2)))
 axis xy
 xlabel('Decay time (sec)')
 ylabel('Noise level')
@@ -55,7 +80,7 @@ ylabel('Noise level')
 % result for fraction neuron
 figure
 subplot(1, 2, 1)
-imagesc(tau_d_list, noise_factor_list, squeeze(mean(mean(cellTypeMat==1, 1), 2)), [0 1])
+imagesc(squeeze(mean(mean(cellTypeMat==1, 1), 2)), [0 1])
 colormap('jet')
 axis xy
 xlabel('Decay time (sec)')
@@ -63,7 +88,7 @@ ylabel('Noise level')
 title('Frac. Mono. Cell')
 
 subplot(1, 2, 2)
-imagesc(tau_d_list, noise_factor_list, squeeze(mean(mean(cellTypeMat==2, 1), 2)), [0 1])
+imagesc(squeeze(mean(mean(cellTypeMat==2, 1), 2)), [0 1])
 colormap('jet')
 axis xy
 xlabel('Decay time (sec)')
@@ -75,33 +100,6 @@ mean(cellTypeMat(:)==1)
 mean(cellTypeMat(:)==2)
 
 
-% short Ca slow virus
-nData  = 4;
-params = DataSetList(nData).params;
-std_r                  = 0.0246;
-median_r               = 0.0505;
-std_d                  = 0.4588;
-median_d               = 1.7064;
-load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
-per_list               = 0.05:0.05:0.95;
-tau_d_list             = icdf('Normal', per_list, 0, 1) * std_d + median_d;
-noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
-
-numNeuron              = length(nDataSet);
-
-cellTypeMat            = nan(numNeuron, numFold, length(noise_factor_list), length(tau_d_list));
-
-for nTau              = 1:length(tau_d_list)
-    spikeDataSet      = getSyntheticSpikeDeconvDataSimpleVersion(nDataSet, median_r, tau_d_list(nTau), params);  
-    for nFF           = 1:length(noise_factor_list) 
-        for nFold     = 1:numFold
-            unitGroup = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
-            cellTypeMat(:, nFold, nFF, nTau) = unitGroup;
-        end
-    end
-end
-
-cellTypeMat6sAAV = cellTypeMat;
 
 % result for single neuron
 figure
