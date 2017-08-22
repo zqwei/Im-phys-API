83cadd6c84bc33174a24f6d8f6ea33d555c60d96
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-21 21:39:25 -0400

    update data to html python file

--- performance_test_code/dataToHtml.py
+++ performance_test_code/dataToHtml.py
@@ -1,7 +1,9 @@
 from glob import glob
 import pandas as pd
 from bokeh.palettes import Category20
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 colors= Category20.get(20)
 names = ['dF/F', 'S2C Linear', 'S2C Hill', 'S2C Sigmoid', 'C2S NWF', 'C2S FOOPSI', 'C2S FRI', 'C2S AR1', 'C2S AR2', 'C2S AR3', 'C2S MCMC', 'C2S Peel']
 
@@ -12,7 +14,6 @@ for nFolder in glob('MV1*'):
     for nFile in range(len(spkFiles)):
         fileName = spkFiles[nFile][:-8]#[len(nFolder)+1:]
         outputFileName = fileName + '.html'
-        output_file(outputFileName)
         spkdb = pd.read_csv(spkFiles[nFile])
         spkdb['spk'] = spkdb['spk'] * -1.0
         dffdb = pd.read_csv(dffFiles[nFile])
@@ -23,4 +24,7 @@ for nFolder in glob('MV1*'):
         p.legend.location = "top_left"
         p.legend.click_policy="hide"
         p.legend.orientation = "horizontal"
-        show(p)
+        html = file_html(p, CDN, "my plot")
+
+        with open(outputFileName, 'w') as f:
+            f.write(html)
