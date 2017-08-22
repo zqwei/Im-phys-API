e689b5894a7569c4a18d16e88bb6eee81bd6cf02
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-21 22:45:57 -0400

    update data to html python file

--- performance_test_code/dataToHtml.py
+++ performance_test_code/dataToHtml.py
@@ -5,7 +5,7 @@ from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 colors= Category20.get(20)
-names = ['dF/F', 'S2C Linear', 'S2C Hill', 'S2C Sigmoid', 'C2S NWF', 'C2S FOOPSI', 'C2S FRI', 'C2S AR1', 'C2S AR2', 'C2S AR3', 'C2S MCMC', 'C2S Peel']
+names = ['dF/F', 'S2C Linear', 'S2C Hill', 'S2C Sigmoid', 'NWF', 'FOOPSI', 'FRI', 'AR1', 'AR2', 'AR3', 'MCMC', 'Peel']
 
 
 for nFolder in glob('MV1*'):
@@ -17,7 +17,7 @@ for nFolder in glob('MV1*'):
         spkdb = pd.read_csv(spkFiles[nFile])
         spkdb['spk'] = spkdb['spk'] * -1.0
         dffdb = pd.read_csv(dffFiles[nFile])
-        p = figure(plot_width=1400, plot_height=500, x_axis_label="Time(sec)", y_axis_label="dF/F")
+        p = figure(plot_width=1000, plot_height=400, x_axis_label="Time(sec)", y_axis_label="dF/F", x_range=(0, 240))
         p.circle(spkdb['spkTime'], spkdb['spk'], line_width=2, color=colors[0], legend='Spikes')
         for name, color in zip(names, range(1, len(names)+1)):
             p.line(dffdb['time'], dffdb[dffdb.columns[color]], line_width=2, color=colors[color], legend=name)
