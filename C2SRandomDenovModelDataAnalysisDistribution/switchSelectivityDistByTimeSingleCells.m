852e62b59949808ecc1de223c8d7a3eff7f47178
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-11-29 09:17:22 -0500

    update cell type code

--- C2SRandomDenovModelDataAnalysisDistribution/switchSelectivityDistByTimeSingleCells.m
+++ C2SRandomDenovModelDataAnalysisDistribution/switchSelectivityDistByTimeSingleCells.m
@@ -13,122 +13,32 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 addpath('../Func');
 setDir;
-% % load ([TempDatDir 'DataListShuffle.mat']);
-% % numFold = 30;
-% % 
-% % % short Ca slow
-% % nData  = 3;
-% % params = DataSetList(nData).params;
-% % std_r                  = 0.0375; %0.0375;
-% % median_r               = 0.0927;
-% % std_d                  = 0.5374; %0.5374;
-% % median_d               = 1.2294;
-% % load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
-% % per_list               = 0.02:0.01:0.98;
-% % tau_d_list             = icdf('Normal', per_list, 0, 1) * std_d + median_d;
-% % noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
-% % numNeuron              = length(nDataSet);
-% % cellTypeMat            = nan(numNeuron, numFold, length(noise_factor_list), length(tau_d_list));
-% % for nTau              = 1:length(tau_d_list)
-% %     spikeDataSet      = getSyntheticSpikeDeconvDataSimpleVersion(nDataSet, median_r, tau_d_list(nTau), params);  
-% %     for nFF           = 1:length(noise_factor_list) 
-% %         for nFold     = 1:numFold
-% %             unitGroup = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
-% %             cellTypeMat(:, nFold, nFF, nTau) = unitGroup;
-% %         end
-% %     end
-% % end
-% % cellTypeMatGP43 = cellTypeMat;
-% % 
-% % % short Ca slow virus
-% % nData  = 4;
-% % params = DataSetList(nData).params;
-% % std_r                  = 0.0246;
-% % median_r               = 0.0505;
-% % std_d                  = 0.4588;
-% % median_d               = 1.7064;
-% % load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
-% % per_list               = 0.05:0.05:0.95;
-% % tau_d_list             = icdf('Normal', per_list, 0, 1) * std_d + median_d;
-% % noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
-% % 
-% % numNeuron              = length(nDataSet);
-% % 
-% % cellTypeMat            = nan(numNeuron, numFold, length(noise_factor_list), length(tau_d_list));
-% % 
-% % for nTau              = 1:length(tau_d_list)
-% %     spikeDataSet      = getSyntheticSpikeDeconvDataSimpleVersion(nDataSet, median_r, tau_d_list(nTau), params);  
-% %     for nFF           = 1:length(noise_factor_list) 
-% %         for nFold     = 1:numFold
-% %             unitGroup = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
-% %             cellTypeMat(:, nFold, nFF, nTau) = unitGroup;
-% %         end
-% %     end
-% % end
-% % cellTypeMat6sAAV = cellTypeMat;
+load ([TempDatDir 'DataListShuffle.mat']);
 
-load('cellTypeMat.mat')
-cellTypeMat = cellTypeMatGP43;
-% result for single neuron
-figure
-nNeuron = 100;
-imagesc(squeeze(mean(cellTypeMat(nNeuron, :, :, :), 2)))
-axis xy
-xlabel('Decay time (sec)')
-ylabel('Noise level')
+% all data is precomputed by code meanSynthEphysTraces.m
 
-% result for fraction neuron
-figure
-subplot(1, 2, 1)
-imagesc(squeeze(mean(mean(cellTypeMat==1, 1), 2)), [0 1])
-colormap('jet')
-axis xy
-xlabel('Decay time (sec)')
-ylabel('Noise level')
-title('Frac. Mono. Cell')
-
-subplot(1, 2, 2)
-imagesc(squeeze(mean(mean(cellTypeMat==2, 1), 2)), [0 1])
-colormap('jet')
-axis xy
-xlabel('Decay time (sec)')
-ylabel('Noise level')
-title('Frac. Multi. Cell')
-
-% mean_value
-mean(cellTypeMat(:)==1)
-mean(cellTypeMat(:)==2)
+indexDatasets = [2, 3, 5, 6];
+% 2: short Ca GP517
+% 3: short Ca slow
+% 5: long Ca fast
+% 6: long Ca slow
 
 
 
-% result for single neuron
-figure
-nNeuron = 100;
-imagesc(tau_d_list, noise_factor_list, squeeze(mean(cellTypeMat(nNeuron, :, :, :), 2)))
-axis xy
-xlabel('Decay time (sec)')
-ylabel('Noise level')
 
-% result for fraction neuron
-figure
-subplot(1, 2, 1)
-imagesc(tau_d_list, noise_factor_list, squeeze(mean(mean(cellTypeMat==1, 1), 2)), [0 1])
-colormap('jet')
-axis xy
-xlabel('Decay time (sec)')
-ylabel('Noise level')
-title('Frac. Mono. Cell')
+per_list               = 0.02:0.01:0.98;
+noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
 
-subplot(1, 2, 2)
-imagesc(tau_d_list, noise_factor_list, squeeze(mean(mean(cellTypeMat==2, 1), 2)), [0 1])
-colormap('jet')
-axis xy
-xlabel('Decay time (sec)')
-ylabel('Noise level')
-title('Frac. Multi. Cell')
-
-% mean_value
-mean(cellTypeMat(:)==1)
-
-save('cellTypeMat', 'cellTypeMat6sAAV', 'cellTypeMatGP43')
-mean(cellTypeMat(:)==2)
+for nData     = indexDatasets
+    for nTau  = 1:99
+        if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
+            load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'],'spikeDataSet')
+            cellTypeMat       = nan(size(spikeDataSet, 1), 99);
+            for nFF           = 1:length(noise_factor_list) 
+              unitGroup       = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
+              cellTypeMat(:, nFF) = unitGroup;
+            end  
+            save([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_cell_type.mat'], 'cellTypeMat');
+        end
+    end    
+end
