c80dc963d670afb89b31b5edaa1b31bc90f152d3
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-11-30 11:55:07 -0500

    rerun the analysis code and update the plots

--- C2SRandomDenovModelDataAnalysisDistribution/peakLocationDistByTimeSingleCells.m
+++ C2SRandomDenovModelDataAnalysisDistribution/peakLocationDistByTimeSingleCells.m
@@ -24,80 +24,86 @@ indexDatasets = [2, 3, 4, 5, 6];
 % 5: long Ca fast
 % 6: long Ca slow
 
-params           = DataSetList(2).params;
-
-sigma            = 0.15 / params.binsize; % 300 ms
-filterLength     = 11;
-filterStep       = linspace(-filterLength / 2, filterLength / 2, filterLength);
-filterInUse      = exp(-filterStep .^ 2 / (2 * sigma ^ 2));
-filterInUse      = filterInUse / sum (filterInUse); 
-
-per_list           = 0.02:0.01:0.98;
-noise_factor_list  = sqrt(icdf('Exponential', per_list, 0.3527));
-timeTag            = 8:60;
-
-for nData     = indexDatasets
-    for nTau  = 1:99
-        if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
-            load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'],'spikeDataSet')
-            peakiness        = nan(99, 1);
-            for nFF          = 1:length(noise_factor_list) 
-                numTimeBin       = size(spikeDataSet(1).unit_yes_trial, 2);
-                yesProfileMatrix = nan(length(spikeDataSet), numTimeBin);
-                noProfileMatrix  = yesProfileMatrix;
-                positivePeak     = false(length(spikeDataSet));
-                for nUnit        = 1:length(spikeDataSet)
-                nUnitData        = spikeDataSet(nUnit).unit_yes_trial;
-                yesUnitData      = getGaussianPSTH (filterInUse, nUnitData, 2);
-                nUnitData        = spikeDataSet(nUnit).unit_no_trial;
-                noUnitData       = getGaussianPSTH (filterInUse, nUnitData, 2);
-                mean_yesUnitData = mean(yesUnitData, 1);
-                mean_noUnitData  = mean(noUnitData, 1);
-                yesSize          = length(spikeDataSet(nUnit).unit_yes_trial_index);
-                noSize           = length(spikeDataSet(nUnit).unit_no_trial_index);
-                numT             = size(nUnitData, 2);
-                mean_rate        = min([mean_yesUnitData, mean_noUnitData]);
-                baseline_rate    = 0;
-                std_yesUnitData  = sqrt(mean_yesUnitData - mean_rate + baseline_rate);
-                std_noUnitData   = sqrt(mean_noUnitData - mean_rate + baseline_rate);
-                ffactor          = noise_factor_list(nFF);
-                yesUnitData      = mean_yesUnitData + ones(size(yesUnitData,1), 1) * std_yesUnitData * ffactor .* randn(yesSize, numT);
-                noUnitData       = mean_noUnitData + ones(size(noUnitData, 1), 1) * std_noUnitData * ffactor .* randn(noSize, numT);
-                yesData          = mean(yesUnitData);
-                noData           = mean(noUnitData);
-                maxData          = max([yesData, noData]);
-                minData          = min([yesData, noData]);
-                rData            = (maxData - minData);
-                yesData          = (yesData - minData)/(maxData - minData);
-                noData           = (noData - minData)/(maxData - minData);
-                yesProfileMatrix(nUnit, :)    = yesData;
-                noProfileMatrix(nUnit, :)     = noData;
-                positivePeak(nUnit)       = mean(yesData(1:8)) <= mean(yesData(9:47)) ...
-                                           || mean(noData(1:8)) <= mean(noData(9:47));
-                end
-                actMat        = [yesProfileMatrix, noProfileMatrix];
-                actMat        = actMat(positivePeak, :);
-                [~, maxId]    = max(actMat, [], 2);
-                countMaxId    = hist(maxId, 1:numTimeBin*2)/size(actMat,1)*100;
-                peakiness(nFF) = std(countMaxId([timeTag, timeTag+77]));
-            end  
-            save([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_peak.mat'], 'peakiness');
-            clear spikeDataSet peakiness
-        end
-    end    
-end
-
+% params           = DataSetList(2).params;
+% 
+% sigma            = 0.15 / params.binsize; % 300 ms
+% filterLength     = 11;
+% filterStep       = linspace(-filterLength / 2, filterLength / 2, filterLength);
+% filterInUse      = exp(-filterStep .^ 2 / (2 * sigma ^ 2));
+% filterInUse      = filterInUse / sum (filterInUse); 
+% 
 % per_list           = 0.02:0.01:0.98;
+% noise_factor_list  = sqrt(icdf('Exponential', per_list, 0.3527));
+% timeTag            = 8:60;
 % 
 % for nData     = indexDatasets
-%     s_mat     = nan(97, 97);
-%     for nTau  = 1:97
+%     for nTau  = 1:99
 %         if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
-%             load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_peak.mat'], 'peakiness')
-%             s_mat(nTau, :) = peakiness(1:97);
+%             load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'],'spikeDataSet')
+%             peakiness        = nan(99, 1);
+%             for nFF          = 1:length(noise_factor_list) 
+%                 numTimeBin       = size(spikeDataSet(1).unit_yes_trial, 2);
+%                 yesProfileMatrix = nan(length(spikeDataSet), numTimeBin);
+%                 noProfileMatrix  = yesProfileMatrix;
+%                 positivePeak     = false(length(spikeDataSet));
+%                 for nUnit        = 1:length(spikeDataSet)
+%                 nUnitData        = spikeDataSet(nUnit).unit_yes_trial;
+%                 yesUnitData      = getGaussianPSTH (filterInUse, nUnitData, 2);
+%                 nUnitData        = spikeDataSet(nUnit).unit_no_trial;
+%                 noUnitData       = getGaussianPSTH (filterInUse, nUnitData, 2);
+%                 mean_yesUnitData = mean(yesUnitData, 1);
+%                 mean_noUnitData  = mean(noUnitData, 1);
+%                 yesSize          = length(spikeDataSet(nUnit).unit_yes_trial_index);
+%                 noSize           = length(spikeDataSet(nUnit).unit_no_trial_index);
+%                 numT             = size(nUnitData, 2);
+%                 mean_rate        = min([mean_yesUnitData, mean_noUnitData]);
+%                 baseline_rate    = 0;
+%                 std_yesUnitData  = sqrt(mean_yesUnitData - mean_rate + baseline_rate);
+%                 std_noUnitData   = sqrt(mean_noUnitData - mean_rate + baseline_rate);
+%                 ffactor          = noise_factor_list(nFF);
+%                 yesUnitData      = mean_yesUnitData + ones(size(yesUnitData,1), 1) * std_yesUnitData * ffactor .* randn(yesSize, numT);
+%                 noUnitData       = mean_noUnitData + ones(size(noUnitData, 1), 1) * std_noUnitData * ffactor .* randn(noSize, numT);
+%                 yesData          = mean(yesUnitData);
+%                 noData           = mean(noUnitData);
+%                 maxData          = max([yesData, noData]);
+%                 minData          = min([yesData, noData]);
+%                 rData            = (maxData - minData);
+%                 yesData          = (yesData - minData)/(maxData - minData);
+%                 noData           = (noData - minData)/(maxData - minData);
+%                 yesProfileMatrix(nUnit, :)    = yesData;
+%                 noProfileMatrix(nUnit, :)     = noData;
+%                 positivePeak(nUnit)       = mean(yesData(1:8)) <= mean(yesData(9:47)) ...
+%                                            || mean(noData(1:8)) <= mean(noData(9:47));
+%                 end
+%                 actMat        = [yesProfileMatrix, noProfileMatrix];
+%                 actMat        = actMat(positivePeak, :);
+%                 [~, maxId]    = max(actMat, [], 2);
+%                 countMaxId    = hist(maxId, 1:numTimeBin*2)/size(actMat,1)*100;
+%                 peakiness(nFF) = std(countMaxId([timeTag, timeTag+77]));
+%             end  
+%             save([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_peak.mat'], 'peakiness');
+%             clear spikeDataSet peakiness
 %         end
 %     end    
-%     
-%     figure;
-%     imagesc(s_mat);
 % end
+
+per_list           = 0.02:0.01:0.98;
+
+for nData     = indexDatasets
+    s_mat     = nan(97, 97);
+    for nTau  = 1:97
+        if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
+            load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_peak.mat'], 'peakiness')
+            s_mat(nTau, :) = peakiness(1:97);
+        end
+    end    
+    
+    figure;
+    imagesc(per_list*100, per_list*100, s_mat);
+    xlabel('percentile noise level')
+    ylabel('percentile \tau_d')
+    axis xy
+    colorbar
+    set(gca, 'TickDir', 'out')
+    setPrint(8, 6, [PlotDir 'DistributionAnanlysis/Peakiness_' DataSetList(nData).name])
+end
--- C2SRandomDenovModelDataAnalysisDistribution/switchSelectivityDistByTimeSingleCells.m
+++ C2SRandomDenovModelDataAnalysisDistribution/switchSelectivityDistByTimeSingleCells.m
@@ -27,54 +27,54 @@ indexDatasets = [2, 3, 4, 5, 6];
 
 
 % % % computing cell type and save
-per_list               = 0.02:0.01:0.98;
-noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
-
-for nData     = indexDatasets
-    params    = DataSetList(nData).params;
-    for nTau  = 1:99
-        if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
-            load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'],'spikeDataSet')
-            cellTypeMat       = nan(size(spikeDataSet, 1), 99);
-            for nFF           = 1:length(noise_factor_list) 
-              unitGroup       = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
-              cellTypeMat(:, nFF) = unitGroup;
-            end  
-            save([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_cell_type.mat'], 'cellTypeMat');
-            clear spikeDataSet cellTypeMat
-        end
-    end    
-end
+% per_list               = 0.02:0.01:0.98;
+% noise_factor_list      = sqrt(icdf('Exponential', per_list, 0.3527));
+% 
+% for nData     = indexDatasets
+%     params    = DataSetList(nData).params;
+%     for nTau  = 1:99
+%         if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
+%             load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'],'spikeDataSet')
+%             cellTypeMat       = nan(size(spikeDataSet, 1), 99);
+%             for nFF           = 1:length(noise_factor_list) 
+%               unitGroup       = getLogPValueTscoreSpikeTimeAve(spikeDataSet, params, noise_factor_list(nFF));
+%               cellTypeMat(:, nFF) = unitGroup;
+%             end  
+%             save([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_cell_type.mat'], 'cellTypeMat');
+%             clear spikeDataSet cellTypeMat
+%         end
+%     end    
+% end
 
 
 % % % plot saved file
-% per_list               = 0.02:0.01:0.98;
-% 
-% for nData     = indexDatasets
-%     monoCell  = nan(99, 99);
-%     multiCell = nan(99, 99);
-%     validTau  = false(99, 1);
-%     for nTau  = 1:99
-%         if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
-%             load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_cell_type.mat']);
-%             monoCell(nTau, :)  = mean(cellTypeMat == 1);
-%             multiCell(nTau, :) = mean(cellTypeMat == 2);
-%             validTau(nTau)     = true;
-%         end
-%     end    
-%     
-%     figure;
-%     subplot(1, 2, 1)
-%     imagesc(per_list(validTau)*100, per_list*100, monoCell(validTau, :), [0 1])
-%     axis xy
-%     xlabel('Noise level (%)')
-%     xlabel('\tau_d (%)')
-%     title('Frac Monophasic neuron')
-%     subplot(1, 2, 2)
-%     imagesc(per_list(validTau), per_list, multiCell(validTau, :), [0 1])
-%     axis xy
-%     xlabel('Noise level (%)')
-%     xlabel('\tau_d (%)')
-%     title('Frac Multiphasic neuron')
-%     
-% end
+per_list               = 0.02:0.01:0.98;
+
+for nData     = indexDatasets
+    monoCell  = nan(97, 97);
+    multiCell = nan(97, 97);
+    validTau  = false(97, 1);
+    for nTau  = 1:99
+        if exist([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '.mat'], 'file')
+            load([TempDatDir 'directDeconv/' DataSetList(nData).name '_Tau' num2str(nTau, '%02d') '_cell_type.mat']);
+            monoCell(nTau, :)  = mean(cellTypeMat(:, 1:97) == 1);
+            multiCell(nTau, :) = mean(cellTypeMat(:, 1:97) == 2);
+            validTau(nTau)     = true;
+        end
+    end    
+    
+    figure;
+    subplot(1, 2, 1)
+    imagesc(per_list(validTau)*100, per_list*100, monoCell(validTau, :), [0 1])
+    axis xy
+    xlabel('Noise level (%)')
+    ylabel('\tau_d (%)')
+    title('Frac Monophasic neuron')
+    subplot(1, 2, 2)
+    imagesc(per_list(validTau), per_list, multiCell(validTau, :), [0 1])
+    axis xy
+    xlabel('Noise level (%)')
+    ylabel('\tau_d (%)')
+    title('Frac Multiphasic neuron')
+    setPrint(8*2, 6, [PlotDir 'DistributionAnanlysis/Cell_type_' DataSetList(nData).name])
+end
