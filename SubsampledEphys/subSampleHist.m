205c1a9d545015ef978c27dc3fac2d292b96bf2b
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-12-11 22:49:05 -0500

    update codes with subsample of neurons at different firing rates

--- SubsampledEphys/subSampleHist.m
+++ SubsampledEphys/subSampleHist.m
@@ -17,24 +17,45 @@ for nUnit   = 1:numNeuron
     frData(nUnit, 2) = mean(mean(nDataSet(nUnit).unit_no_trial(:, timePeriod)))*2.6;
 end
 
+frData      = mean(frData, 2);
+
 empY        = hist(mean(frData, 2), 0:1:200); % mean at 5.254 Hz
 empY        = empY/numNeuron;
 
+numFold     = 30;
+
 % low firing rate 1Hz
 
-for frThres = [1 4 10] * 2.6 % spike count in this case
-    Y           = poisspdf(0:1:200,frThres);
-    Y           = Y/sum(Y);
-    empRand     = rand(numNeuron, 1);
-    subSampleratio = Y./empY;
-    empFR       = ceil(mean(frData, 2));
-    validData   = empFR < 40+frThres;
-    empRatio    = subSampleratio(empFR);
-    countsNum   = hist(mean(frData(empRand < empRatio' & validData, :), 2), 0:1:200);
+for frThres = [1 4 10] % spike count in this case
+%     Y           = poisspdf(0:1:200,frThres * 2.6 );
+%     Y           = Y/sum(Y);
+%     subSampleratio = Y./empY;
+%     empFR       = ceil(mean(frData, 2));
+%     validData   = empFR < 40+frThres * 2.6 ;
+%     empRatio    = subSampleratio(empFR);
+%     
+    totCounts   = 0;
+    meanFR      = 0;
+%     
+%     validMat    = false(numNeuron, numFold);
+    load(['validMat_' num2str(frThres, '%02d')], 'validMat')
+    for nFold   = 1:numFold
+        empRand     = rand(numNeuron, 1);
+%         validMat(:, nFold) = empRand < empRatio' & validData;
+        countsNum   = hist(frData(validMat(:, nFold)), 0:1:200);
+        countsNum   = countsNum/sum(countsNum);
+        totCounts   = totCounts + countsNum/numFold;
+        meanFR      = meanFR + mean(frData(validMat(:, nFold)))/numFold;
+    end
+    
     hold on
-    stairs((0:1:200)/2.6, countsNum/sum(countsNum), 'linewid', 2.0)
-    disp(mean(mean(frData(empRand < empRatio' & validData, :), 2))/2.6);  
+    stairs((0:1:200)/2.6, totCounts, 'linewid', 1.0)
+    disp(meanFR/2.6); 
+%     save(['validMat_' num2str(frThres, '%02d')], 'validMat')
 end
 
 xlim([0 25])
-
+xlabel('Firing rate (/s)')
+ylabel('Frac. neuron')
+set(gca, 'TickDir', 'out')
+setPrint(8, 6, 'Firing_rate_distribution', 'pdf')
