db34c82b158782fc159d2395da0ccdd29b2563cf
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-01-04 13:46:11 -0500

    update s2c figure

--- S2CModelDataAnalysisDistribution/allAnalysisFigure.m
+++ S2CModelDataAnalysisDistribution/allAnalysisFigure.m
@@ -14,29 +14,39 @@ Y             = reshape([analysisMat.pTaud], 30, 30);
 Z             = reshape([analysisMat.sizeGroup], 3, 900);
 figure;
 subplot(1, 3, 1)
+hold on
 h = pcolor(X, Y, reshape(Z(2,:), 30, 30)/720-0.66);
 set(h, 'EdgeColor', 'none');
 colormap(french(128, 2))
 caxis([-0.2 0.2])
 colorbar
+plot(55, 55, '*k')
+hold off
 box off
 xlim([0 100])
 ylim([0 100])
 xlabel('Frac. Nonlinearity')
 ylabel('Frac. \tau_d')
 set(gca, 'TickDir', 'out')
+
+
 subplot(1, 3, 2)
+hold on
 h = pcolor(X, Y, reshape(Z(3,:), 30, 30)/720-0.05);
 colormap(french(128, 2))
-caxis([-0.1 0.1])
+caxis([-0.05 0.05])
 set(h, 'EdgeColor', 'none');
 colorbar
+plot(55, 55, '*k')
+hold off
 box off
 xlim([0 100])
 ylim([0 100])
 xlabel('Frac. Nonlinearity')
 ylabel('Frac. \tau_d')
 set(gca, 'TickDir', 'out')
+
+
 Z             = [analysisMat.peakiness];
 subplot(1, 3, 3)
 h = pcolor(X, Y, reshape(Z, 30, 30)-0.49);
