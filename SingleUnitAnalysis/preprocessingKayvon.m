4583b9af10afa2cbbb7feebe3bb768ce10537b9f
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-03-12 16:13:11 -0400

    add code to add Kayvon's data

---
+++ SingleUnitAnalysis/preprocessingKayvon.m
@@ -0,0 +1,13 @@
+fileDirs = {'pt203', 'pt204'};
+copyTarDir = '/Users/dalin/Documents/My_Research/Dataset/ALM_Svoboda_Lab_Data/Data_In_Use/Dataset_Comparison/ImagingData/delay1f2t/';
+
+for nDir = fileDirs
+    localDir  = ['/Volumes/My Drive/ALM_Recording_Pole_Task_Svoboda_Lab/Kayvon _ALM_GP43/' nDir{1}];
+    fileLists = dir([localDir '/*small*.mat']);
+    for nfile = 1:length(fileLists)
+        load([localDir '/' fileLists(nfile).name])
+        if isfield(dat_small, 'roi')
+            copyfile([localDir '/' fileLists(nfile).name], [copyTarDir nDir{1} '_' fileLists(nfile).name])
+        end
+    end
+end
