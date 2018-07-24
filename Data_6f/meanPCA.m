fd40bda4174460cc9f9b552aef780565bd1b287d
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-07-23 20:53:35 -0400

    6f data analysis

---
+++ Data_6f/meanPCA.m
@@ -0,0 +1,78 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dPCA and PCA across trials
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath('../Func');
+setDir;
+load ([TempDatDir 'DataListShuffle.mat']);
+
+combinedParams = {{1}, {2}, {[1 2]}};
+margNames      = {'Stim', 'Time', 'Inter'};
+numTrials      = 100;
+numFold        = 100;
+if ~exist([PlotDir 'CollectedUnitsPCA'],'dir')
+    mkdir([PlotDir 'CollectedUnitsPCA'])
+end
+
+
+numComps       = 10;
+cmap = [         0    0.4470    0.7410
+    0.8500    0.3250    0.0980
+    0.9290    0.6940    0.1250
+    0.4940    0.1840    0.5560
+    0.4660    0.6740    0.1880
+    0.3010    0.7450    0.9330
+    0.6350    0.0780    0.1840];
+
+ROCThres            = 0.70;
+
+
+for nData      = [1 10]
+    if ~exist([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'], 'file')
+        load([TempDatDir DataSetList(nData).name '.mat'])
+        neuronRemoveList = false(length(nDataSet), 1);
+        selectedNeuronalIndex = DataSetList(nData).ActiveNeuronIndex';
+    else
+        load([TempDatDir DataSetList(nData).name '_withOLRemoval.mat'])
+        selectedNeuronalIndex = DataSetList(nData).ActiveNeuronIndex(~neuronRemoveList)';
+    end
+    
+    depth_list          = [nDataSet.depth_in_um]';
+    
+    oldDataSet          = nDataSet;
+    selectedNeuronalIndex = selectedHighROCneurons(oldDataSet, DataSetList(nData).params, ROCThres, selectedNeuronalIndex);
+    selectedNeuronalIndex = selectedNeuronalIndex & depth_list < 471;
+    nDataSet              = oldDataSet(selectedNeuronalIndex);
+    decodability          = zeros(numFold, size(nDataSet(1).unit_yes_trial,2)); 
+    
+    evMat              = zeros(numFold, length(combinedParams), numComps);
+    firingRates        = generateDPCAData(nDataSet, numTrials);
+    firingRatesAverage = nanmean(firingRates, ndims(firingRates));
+    pcaX               = firingRatesAverage(:,:);
+    firingRatesAverage = bsxfun(@minus, firingRatesAverage, mean(pcaX,2));
+    pcaX               = bsxfun(@minus, pcaX, mean(pcaX,2));
+    Xmargs             = dpca_marginalize(firingRatesAverage, 'combinedParams', combinedParams, 'ifFlat', 'yes');
+    totalVar           = sum(sum(pcaX.^2));
+    [~, S, Wpca] = svd(pcaX');
+
+    PCAmargVar         = zeros(length(combinedParams), length(nDataSet));
+    for i=1:length(Xmargs)
+        PCAmargVar(i,:) = sum((Wpca' * Xmargs{i}).^2, 2)' / totalVar;
+    end
+    sum(sum(PCAmargVar, 1)>0.01)
+
+    figure;
+    bar(1:numComps, PCAmargVar(:, 1:numComps)','stacked', 'edgecolor', 'none')
+    box off
+    xlim([0 numComps+0.5])
+    ylim([0 0.5])
+    xlabel('Component index')
+    ylabel('frac. EV per PC')
+    colormap(cmap(1:3, :))
+    set(gca, 'xTick', 0:5:10)
+    set(gca, 'TickDir', 'out')
+    setPrint(8, 6, [PlotDir 'CollectedUnitsPCA/CollectedUnitsPCA_' DataSetList(nData).name])  
+    legend({'Trial type', 'Time', 'Other'})
+    legend('boxoff')
+    setPrint(8, 6, [PlotDir 'CollectedUnitsPCA/' DataSetList(nData).name '_pca'], 'svg')   
+end
---
+++ Data_6f/peakLocationDistrPosNeuronS2C.m
@@ -0,0 +1,62 @@
+addpath('../Func');
+setDir;
+load ([TempDatDir 'DataListS2C6fModel.mat']);
+
+
+for nData     = 2
+    load([TempDatDir DataSetList(nData).name '.mat'])
+    numTimeBin          = size(nDataSet(1).unit_yes_trial, 2);
+    yesProfileMatrix    = nan(length(nDataSet), numTimeBin);
+    noProfileMatrix     = yesProfileMatrix;
+    positivePeak        = false(length(nDataSet));
+    for nUnit        = 1:length(nDataSet)
+        yesData      = mean(nDataSet(nUnit).unit_yes_trial);
+        noData       = mean(nDataSet(nUnit).unit_no_trial);
+        maxData      = max([yesData, noData]);
+        minData      = min([yesData, noData]);
+        rData        = (maxData - minData);
+        yesData      = (yesData - minData)/(maxData - minData);
+        noData       = (noData - minData)/(maxData - minData);
+        yesProfileMatrix(nUnit, :)    = yesData;
+        noProfileMatrix(nUnit, :)     = noData;
+        positivePeak(nUnit)       = mean(yesData(1:8)) <= mean(yesData(9:47)) ...
+                                   || mean(noData(1:8)) <= mean(noData(9:47));
+    end
+    actMat        = [yesProfileMatrix, noProfileMatrix];
+    actMat        = actMat(positivePeak, :);
+    [~, maxId]    = max(actMat, [], 2);
+
+    timeStep  = DataSetList(nData).params.timeSeries;
+    numTime   = length(timeStep);
+    polein    = DataSetList(nData).params.polein;
+    poleout   = DataSetList(nData).params.poleout;
+    timePoints  = timePointTrialPeriod(params.polein, params.poleout, params.timeSeries);
+    timeTag   = timePoints(2):timePoints(4)+13; % sample to response
+    numTime   = length(timeTag);
+    
+    countMaxId = hist(maxId, 1:numTimeBin*2)/size(actMat,1)*100;
+    std(countMaxId([timeTag, timeTag+numTimeBin]))%/mean(countMaxId([timeTag, timeTag+77]))
+    [bootstat,bootsam] = bootstrp(1000,@std,countMaxId([timeTag, timeTag+77]));
+%     mean(bootstat)
+    std(bootstat)
+%     [h, p] = ttest(bootstat)
+    figure;
+    hold on;
+%     stairs(timeStep, countMaxId(1:numTimeBin), '-', 'linewid', 1.0, 'color', [0.7 0 0])
+    bplot = bar(timeStep, countMaxId(1:numTimeBin), 1, 'facecolor', 'b', 'edgecolor', 'none');
+    bplot.FaceAlpha = 0.5;
+%     stairs(timeStep, countMaxId(1+numTimeBin:end), '-', 'linewid', 1.0, 'color', [0 0 0.7]);
+    bplot = bar(timeStep, countMaxId(1+numTimeBin:end), 1, 'facecolor', 'r', 'edgecolor', 'none');
+    bplot.FaceAlpha = 0.5;
+    xlim([timeStep(1) 2])
+    ylim([0 8])
+    gridxy ([polein, poleout, 0],[1/(numTimeBin-8)/2*100], 'Color','k','Linestyle','--','linewid', 1.0)
+    box off
+    ylabel('% Max peak')
+    xlabel('Time')
+    set(gca, 'TickDir', 'out')
+    hold off
+    setPrint(8, 3, [PlotDir 'SingleUnitsPeakLocation\SingleUnitsMaxLocationPosNeuron_' DataSetList(nData).name])
+end
+
+close all
