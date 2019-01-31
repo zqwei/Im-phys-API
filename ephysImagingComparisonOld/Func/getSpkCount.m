bea9b5500eb513d7aaf3c3bdec8878eef8cb6131
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-01-31 00:05:04 -0500

    reorganize the API folder and move the unused files to the depreciates

--- ephysImagingComparisonOld/Func/getSpkCount.m
+++
@@ -1,4 +0,0 @@
-function spkCount     = getSpkCount(nData, spkCountTime, spkCountDur, trialType)
-    averagedBinData   = mean(nData.(trialType), 1);
-    spkCount          = sum(averagedBinData(spkCountTime))/spkCountDur;
-end
--- ephysImagingComparisonOld/Func/plotFuncs/setColorbar.m
+++
@@ -1,8 +0,0 @@
-function setColorbar(cmap, cmin, cmax, textLabel, fileName)
-
-    figure;
-    axis off
-    colormap(cmap);
-    caxis([roundto(cmin) roundto(cmax)]);
-    setColorbarOnlySize(textLabel)
-    setPrint(4, 6, [fileName 'Colorbar']);
